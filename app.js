ba0402b24fe400000f1a4b8d09d0b1c4360b56a0
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-01-26 04:21:16 +0430

    add JavaScript Linters, checked linters, improve nav functionality

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const navSlide = () => {
-    const burger = document.querySelector('.humberger-menu');
-    const nav = document.querySelector('.primary-navigation');
-    const logo = document.querySelector('.logo a');
-    
+  const burger = document.querySelector('.humberger-menu');
+  const nav = document.querySelector('.primary-navigation');
 
-    burger.addEventListener('click', ()=>{
-        nav.classList.toggle('nav-active');
-        nav.style.position = 'fixed'
-        logo.style.display = 'none'
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
+  });
+  document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+    nav.classList.remove('nav-active');
+    burger.classList.remove('toggle');
+  }));
+};
 
-        burger.classList.toggle('toggle');
-      
-    });
-    
-}
 navSlide();

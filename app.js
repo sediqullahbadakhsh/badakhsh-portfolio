654c612443e6167d2350b75e95570cc520fe1c88
fed1k <allamurotovcoder@mail.ru>
2022-01-26 03:04:31 +0500

    add hidden functionality to logo

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const navSlide = () => {
     const burger = document.querySelector('.humberger-menu');
     const nav = document.querySelector('.primary-navigation');
+    const logo = document.querySelector('.logo a');
     
 
     burger.addEventListener('click', ()=>{
         nav.classList.toggle('nav-active');
+        nav.style.position = 'fixed'
+        logo.style.display = 'none'
 
         burger.classList.toggle('toggle');
       

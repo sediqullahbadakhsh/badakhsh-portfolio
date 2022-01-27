01f420c266e3a806cec4e1a74cf5fd3ea0b6e2a8
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-01-27 15:49:23 +0430

    check linter issues

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 const navSlide = () => {
-  const burger = document.querySelector(".humberger-menu");
-  const nav = document.querySelector(".primary-navigation");
+  const burger = document.querySelector('.humberger-menu');
+  const nav = document.querySelector('.primary-navigation');
 
-  burger.addEventListener("click", () => {
-    nav.classList.toggle("nav-active");
-    burger.classList.toggle("toggle");
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
   });
-  document.querySelectorAll(".nav-link").forEach((n) =>
-    n.addEventListener("click", () => {
-      nav.classList.remove("nav-active");
-      burger.classList.remove("toggle");
-    })
-  );
+  document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+    nav.classList.remove('nav-active');
+    burger.classList.remove('toggle');
+  }));
 };
 
 navSlide();
-const modalBtn = document.querySelector(".project-button");
-const modalBg = document.querySelector(".modal-section");
-const modalClose = document.querySelector(".close-btn");
+const modalBtn = document.querySelector('.project-button');
 
-modalBtn.addEventListener("click", function () {
-  modalBg.classList.add("modal-active");
+const modalBg = document.querySelector('.modal-section');
+const modalClose = document.querySelector('.close-btn');
+
+modalBtn.addEventListener('click', () => {
+  modalBg.classList.add('modal-active');
 });
-modalClose.addEventListener("click", function () {
-  modalBg.classList.remove("modal-active");
+modalClose.addEventListener('click', () => {
+  modalBg.classList.remove('modal-active');
 });

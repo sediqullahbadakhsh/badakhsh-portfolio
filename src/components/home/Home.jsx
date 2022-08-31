1523dcb7c052e810433fb47e822008d78d1c6a13
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-08-31 16:30:14 +0430

    configure linters

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import {
+  FaGithub,
+  FaTwitterSquare,
+  FaLinkedin,
+  FaAngellist,
+} from 'react-icons/fa';
 
-const Home = () => {
-  return <div>Home</div>;
-};
+const Home = () => (
+  <div>
+    <h2>Hi I am</h2>
+    <h1>Sediqullah Badakhsh</h1>
+    <h2>I am a full stack Web Developer</h2>
+    <p>
+      I can help you build a product , feature or website Look through some of
+      my work and experience! If you like what you see and have a project you
+      need coded, don&apos;t hestiate to contact me.
+    </p>
+    <FaGithub />
+    <FaTwitterSquare />
+    <FaLinkedin />
+    <FaAngellist />
+  </div>
+);
 
 export default Home;

c1adea0d503cc8308bc38f737b049e1e18a5b763
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-09-01 20:09:46 +0430

    add home content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.scss';
-import About from './components/about/About';
-import Contact from './components/contact/Contact';
-import Home from './components/home/Home';
-import Navbar from './components/navbar/Navbar';
-import Portfolio from './components/portfolio/Portfolio';
-import Resume from './components/resume/Resume';
+import "./App.scss";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Home from "./components/home/Home";
+import Navbar from "./components/navbar/Navbar";
+import Portfolio from "./components/portfolio/Portfolio";
+import Resume from "./components/resume/Resume";
 
 function App() {
   return (
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,25 +1,34 @@
-import React from 'react';
+import "./Home.scss";
+import React from "react";
 import {
   FaGithub,
   FaTwitterSquare,
   FaLinkedin,
   FaAngellist,
-} from 'react-icons/fa';
+  FaDownload,
+  FaArrowDown,
+} from "react-icons/fa";
 
 const Home = () => (
-  <div>
-    <h2>Hi I am</h2>
-    <h1>Sediqullah Badakhsh</h1>
-    <h2>I am a full stack Web Developer</h2>
-    <p>
-      I can help you build a product , feature or website Look through some of
-      my work and experience! If you like what you see and have a project you
-      need coded, don&apos;t hestiate to contact me.
-    </p>
-    <FaGithub />
-    <FaTwitterSquare />
-    <FaLinkedin />
-    <FaAngellist />
+  <div className="home_container">
+    <div className="home_social">
+      <FaGithub />
+      <FaTwitterSquare />
+      <FaLinkedin />
+      <FaAngellist />
+      <FaDownload />
+    </div>
+    <div className="home_intro">
+      <h2>Hi I am</h2>
+      <h1>Sediqullah Badakhsh</h1>
+      <h2>I am a Full Stack Web Developer</h2>
+      <p>
+        I can help you build a product , feature or website Look through some of
+        my work and experience! If you like what you see and have a project you
+        need coded, don&apos;t hestiate to contact me.
+      </p>
+      <FaArrowDown />
+    </div>
   </div>
 );
 

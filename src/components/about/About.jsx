c3da32b1cbc6b26ef8a2cfb0382830559759871b
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-09-02 14:00:05 +0430

    about basic content and style

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React from "react";
+import "./About.scss";
+import sediq from "../../assets/sediq-image.jpg";
 
-const About = () => {
-  return <div>About</div>;
-};
+const About = () => (
+  <div className="about-container" id="about">
+    <div className="about-image">
+      <img src={sediq} alt="sediq" />
+    </div>
+    <div className="about-content">
+      <h1>About Me</h1>
+      <p>
+        I am full stack web developer studied Microverse full-stack web
+        development program remotly, while learning how to code, developed 10s
+        of projects using HTML, CSS, JavaScrip, React, Redux, Ruby on Rails, and
+        PostGreSql, collaborating, pair programming, providing code reviews,
+        testing, mentoring, and...
+      </p>
+      <table>
+        <tr>
+          <td>Name:</td>
+          <td>Sediqullah Badakhsh</td>
+        </tr>
+        <tr>
+          <td>Email:</td>
+          <td>sediqullah.badakhsh@gmail.com</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+);
 
 export default About;

0edb883f33ce1e1f9c6cc2ea35fec83af26a4054
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-09-06 02:26:43 +0430

    apply basic style to contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,27 +1,28 @@
 import React from "react";
+import "./Contact.scss";
 
 const Contact = () => (
-  <div id="contact-container">
+  <div className="contact-container" id="contact">
     <h5>get in touch</h5>
     <h2>Contact Me</h2>
     <div className="contact-details">
       <div className="contact-options">
         <article className="contact-option">
           <h4>Email</h4>
-          <h4>Email</h4>
+          <h4>sediqullah.badakhsh@gmail.com</h4>
           <a href="mailto:something@gmail.com" target="_blank" rel="noreferrer">
-            sedn a mess
+            sedn a Message
           </a>
         </article>
         <article className="contact-option">
           <h4>Messener</h4>
-          <h4>Messenger icon</h4>
+          <h4>Sediqullah Badakhsh</h4>
           <a
             href="https://m.me/sediqullah.badakhsh.5"
             target="_blank"
             rel="noreferrer"
           >
-            send a mess
+            send a Message
           </a>
         </article>
       </div>

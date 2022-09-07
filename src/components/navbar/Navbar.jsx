37dab05e6e7f0b17f9c598668934afc81ea708f0
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-09-07 23:04:53 +0430

    make responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,23 +12,23 @@ const Navbar = () => (
   <nav>
     <a href="#home" data-to-scrollspy-id="home">
       <AiOutlineHome />
-      Home
+      <span className="hidden"> Home</span>
     </a>
     <a href="#about" data-to-scrollspy-id="about">
       <AiOutlineUser />
-      About
+      <span className="hidden"> About</span>
     </a>
     <a href="#resume" data-to-scrollspy-id="resume">
       <AiOutlineBook />
-      Resume
+      <span className="hidden"> Resume</span>
     </a>
     <a href="#portfolio" data-to-scrollspy-id="portfolio">
       <AiOutlineProject />
-      Portfolio
+      <span className="hidden"> Portfolio</span>
     </a>
     <a href="#contact" data-to-scrollspy-id="contact">
       <AiOutlineContacts />
-      Contact
+      <span className="hidden"> Contact</span>
     </a>
   </nav>
 );

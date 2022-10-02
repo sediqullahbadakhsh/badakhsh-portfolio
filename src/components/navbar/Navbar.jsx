2abe18afdbe52b102e68c827435462ed45032794
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-10-03 02:47:19 +0430

    refactor navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,35 +1,13 @@
 import React from "react";
 import "./Navbar.scss";
-import {
-  AiOutlineHome,
-  AiOutlineUser,
-  AiOutlineBook,
-  AiOutlineProject,
-  AiOutlineContacts,
-} from "react-icons/ai";
+import { accordiondata } from "./NavData";
+import NavList from "./NavList";
 
 const Navbar = () => (
   <nav>
-    <a href="#home" data-to-scrollspy-id="home">
-      <AiOutlineHome />
-      <span className="hidden"> Home</span>
-    </a>
-    <a href="#about" data-to-scrollspy-id="about">
-      <AiOutlineUser />
-      <span className="hidden"> About</span>
-    </a>
-    <a href="#resume" data-to-scrollspy-id="resume">
-      <AiOutlineBook />
-      <span className="hidden"> Resume</span>
-    </a>
-    <a href="#portfolio" data-to-scrollspy-id="portfolio">
-      <AiOutlineProject />
-      <span className="hidden"> Portfolio</span>
-    </a>
-    <a href="#contact" data-to-scrollspy-id="contact">
-      <AiOutlineContacts />
-      <span className="hidden"> Contact</span>
-    </a>
+    {accordiondata.map(({ url, content }) => (
+      <NavList url={url} content={content} key={content} />
+    ))}
   </nav>
 );
 

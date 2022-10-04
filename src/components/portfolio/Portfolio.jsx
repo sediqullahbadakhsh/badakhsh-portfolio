b58b3740f0d5589616d9536829dc9bf2d3432350
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-10-04 16:19:56 +0430

    update portfolio content

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,33 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "./Modal";
 import "./Portfolio.scss";
 import list from "./projectList";
+import textShortner from "./textShortner";
 
-const Portfolio = () => (
-  <div className="portfolio-container" id="Projects">
-    <div className="portfolio-header">
-      <h2>Recent Projects</h2>
-      <a href="#/">More Projects</a>
-    </div>
-    <div className="portfolio-content">
-      {list.map((item) => (
-        <div key={item.id} className="portfolio-card">
-          <img className="card-image" src={item.image} alt={item.title} />
-          <h3 className="card-title">{item.title}</h3>
-          <div className="card-techs">
-            {item.tech.map((tec) => (
-              <p key={tec} className="techs">
-                {tec}
-              </p>
-            ))}
+const Portfolio = () => {
+  const [active, setActive] = useState(false);
+  const hundleClick = () => setActive(!active);
+
+  return (
+    <div className="portfolio-container" id="Projects">
+      <div className="portfolio-header">
+        <h2 className="heading">Featured Projects</h2>
+        <a href="#/" className="shadow_effect view_more_btn">
+          View more...
+        </a>
+      </div>
+      <div className="portfolio-content">
+        {list.map((item) => (
+          <div key={item.id} className="portfolio-card">
+            <img className="card-image" src={item.image} alt={item.title} />
+            <h3 className="card-title">{item.title}</h3>
+            <p>{item.date}</p>
+            <p>{textShortner(item.description)}</p>
+            <div className="pr-card-btn">
+              <a href="#/" onClick={hundleClick}>
+                View Details
+              </a>
+            </div>
+            <div className={active === true ? "active" : "hidden"}>
+              <Modal hundleClick={hundleClick} date={item.date} />
+            </div>
           </div>
-          <div className="card-link">
-            <a href={item.github}>Github</a>
-            <a href={item.live}>Preview</a>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Portfolio;

6d50aacbf9945d6ab46dd93e9d1d836e287ab8fa
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-10-04 20:43:48 +0430

    render selected data

--- src/components/portfolio/Modal.jsx
+++ src/components/portfolio/Modal.jsx
@@ -1,14 +1,32 @@
 /* eslint-disable */
 import React, { useState } from "react";
 
-const Modal = ({ hundleClick }) => {
+const Modal = ({
+  hundleClick,
+  image,
+  title,
+  github,
+  live,
+  tech,
+  date,
+  description,
+}) => {
   const [deactive, setDeactive] = useState(true);
   const onClick = () => {
     hundleClick(setDeactive(!deactive));
   };
+
   return (
     <div className="modal">
       <div>modal</div>
+      <p>{image}</p>
+      <p>{title}</p>
+      <p>{github}</p>
+      <p>{tech && tech.map((t) => <p>{t}</p>)}</p>
+      <p>{live}</p>
+      <p>{date}</p>
+      <p>{description}</p>
+
       <button onClick={onClick}>Close</button>
     </div>
   );
